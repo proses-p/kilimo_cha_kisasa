8ea9cc8118ab0fe424f06a94349e6a2283e75f3c
proses-p <prosesprojestus0@gmail.com>
2026-05-26 17:36:37 +0300

    update cors file

--- config/cors.php
+++ config/cors.php
@@ -32,6 +32,12 @@ return [
     'supports_credentials' => false,
 
 
-    'allowed_origins' => ['http://localhost:5173'],
+    
+    
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://kilimo-cha-kisasa-frontend.vercel.app',
+        
+     ],
 
 ];
